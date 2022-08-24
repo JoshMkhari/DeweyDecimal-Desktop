fdd40ba55fafdc168377c0e154c90546551e05bc
Josh <njmkhari@gmail.com>
2022-08-24 20:37:04 +0200

    Rectangles stroke change colour Push is working but pushing wrong item xD

--- Views/ReplaceBooksView.xaml.cs
+++ Views/ReplaceBooksView.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace JoshMkhariPROG7312Game.Views
 {
@@ -13,14 +15,22 @@ namespace JoshMkhariPROG7312Game.Views
     {
         private int _activatedBlockCount;
 
+        private bool[] _rectOrders = new bool [4];
         private int[][] _rectanglesArr = new int[4][]; //https://www.geeksforgeeks.org/c-sharp-jagged-arrays/
         // Creating 2 Stacks of Integers https://www.tutorialsteacher.com/csharp/csharp-stack
+        
         private Stack<int> _callNumbersTop = new Stack<int>();//To store initial numbers in top rectangle
         private Stack<int> _callNumbersBottom = new Stack<int>();//To store initial numbers in bottom rectangle
+        private Stack<int> _callNumbersLeft = new Stack<int>();//To store initial numbers in left rectangle
+        private Stack<int> _callNumbersRight = new Stack<int>();//To store initial numbers in right rectangle
 
+
+        private int _callRecToMove;
         public ReplaceBooksView()
         {
+            
             InitializeComponent();
+            _callRecToMove = 0;
             _activatedBlockCount = 0;
             List<int> callNumbers = new List<int>();//To store initial 10 values
             //To generate random numbers https://www.tutorialsteacher.com/articles/generate-random-numbers-in-csharp
@@ -53,11 +63,12 @@ namespace JoshMkhariPROG7312Game.Views
                 }
             } while (callNumbers.Count < 10);//Repeat until 10 random numbers have been stored
 
-            bool isEmptyRect;
+            
             bool topFull = false;
             bool botFull = false;
             do
             {
+                bool isEmptyRect;
                 int rectChoice = rnd.Next(2);//stores random integers < 2
                 if (rectChoice == 0)//Top rectangle was chosen
                 {
@@ -145,33 +156,342 @@ namespace JoshMkhariPROG7312Game.Views
 
         private void selectTopRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            bool isEmptyRect = !_callNumbersTop.Any();//check if the list is empty
             if (_activatedBlockCount == 0)//This is start block
             {
-
                 //check num of blocks within rect
-
+                if (isEmptyRect)//No more blocks to move
+                {
+                    SolidColorBrush blackBrush = new SolidColorBrush(Colors.Red);
+                    TopRectStrokeChange(blackBrush);
+                }
+                else
+                {
+                    _callRecToMove = 0;//Represents Top
+                    SolidColorBrush blackBrush = new SolidColorBrush(Colors.Gold);
+                    this.RegisterName("MySolidColorBorderBrush", blackBrush);
+                    selectTopRect.StrokeThickness = 3;
+                    selectTopRect.Stroke = blackBrush;
+                }
+                
                 _activatedBlockCount++;//Program now knows start, waiting for destination
             }
             else //this is destination block
             {
-
+                //check num of blocks within rect
+                
+                if (isEmptyRect)//can add block
+                {
+                    _callNumbersTop.Push(PopCallBlock(_callRecToMove));
+                }
+                else
+                {
+                    if (_callNumbersTop.Count < 5)//If there is still space for the block
+                    {
+                        if (_callNumbersTop.Count == 1)//If there is only one block inside
+                        { 
+                            _callNumbersTop.Push(PopCallBlock(_callRecToMove)); //add second block
+                            if (_callNumbersTop.ElementAt(0) > _callNumbersTop.ElementAt(1))
+                            {
+                                _rectOrders[0] = false;//Store Descending for Top Rectangle
+                            }
+                            else
+                            {
+                                _rectOrders[0] = true;//Store Ascending for Top Rectangle
+                            }
+                        }
+                        else
+                        {
+                            if (PopCallBlock(_callRecToMove) > _callNumbersTop.Peek())//If new addition is greater than top num
+                            {
+                                _callNumbersTop.Push(PopCallBlock(_callRecToMove)); //add block
+                            }
+                            else
+                            {
+                                SolidColorBrush blackBrush = new SolidColorBrush(Colors.Red);
+                                TopRectStrokeChange(blackBrush);
+                            }
+                            //What is my order
+                            if (_rectOrders[0])//If ascending
+                            {
+                                if (PopCallBlock(_callRecToMove) > _callNumbersTop.Peek())//If new addition is greater than top num
+                                {
+                                    _callNumbersTop.Push(PopCallBlock(_callRecToMove)); //add block
+                                }
+                                else
+                                {
+                                    SolidColorBrush blackBrush = new SolidColorBrush(Colors.Red);
+                                    TopRectStrokeChange(blackBrush);
+                                }
+                            }
+                            //Does it match my order?
+                        }
+                    }
+                    else
+                    {
+                        SolidColorBrush blackBrush = new SolidColorBrush(Colors.Red);
+                        TopRectStrokeChange(blackBrush);
+                    }
+                }
             }
-            //Call method to activate block
         }
 
+        private void TopRectStrokeChange(SolidColorBrush blackBrush)
+        {
+            this.RegisterName("MySolidColorBorderBrush", blackBrush);
+            selectTopRect.StrokeThickness = 3;
+            selectTopRect.Stroke = blackBrush;
+        }
+        
         private void selectBottomRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            bool isEmptyRect = !_callNumbersBottom.Any();//check if the list is empty
+            if (_activatedBlockCount == 0)//This is start block
+            {
+                //check num of blocks within rect
+                if (isEmptyRect)//No more blocks to move
+                {
+                    MessageBox.Show("Code X to appear and say empty");
+                }
+                else
+                {
+                    _callRecToMove = 1;//Represents Bottom
+                }
+                
+                _activatedBlockCount++;//Program now knows start, waiting for destination
+            }
+            else //this is destination block
+            {
+                //check num of blocks within rect
+                
+                if (isEmptyRect)//can add block
+                {
+                    _callNumbersBottom.Push(PopCallBlock(_callRecToMove));
+                }
+                else
+                {
+                    if (_callNumbersBottom.Count < 5)//If there is still space for the block
+                    {
+                        if (PopCallBlock(_callRecToMove) > _callNumbersTop.Peek())//If new addition is greater than top num
+                        {
+                            _callNumbersTop.Push(PopCallBlock(_callRecToMove)); //add block
+                        }
+                        else
+                        {
+                            MessageBox.Show("This is an ascending list only X");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Space Error");
+                    }
+                }
+            }
         }
 
         private void selectRightRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-
+        { 
+            bool isEmptyRect = !_callNumbersRight.Any();//check if the list is empty
+            if (_activatedBlockCount == 0)//This is start block
+            {
+                //check num of blocks within rect
+                if (isEmptyRect)//No more blocks to move
+                {
+                    MessageBox.Show("Code X to appear and say empty");
+                }
+                else
+                {
+                    _callRecToMove = 3;//Represents Right
+                }
+                
+                _activatedBlockCount++;//Program now knows start, waiting for destination
+            }
+            else //this is destination block
+            {
+                //check num of blocks within rect
+                
+                if (isEmptyRect)//can add block
+                {
+                    SolidColorBrush blackBrush = new SolidColorBrush(Colors.Blue);
+                    //this.RegisterName("MySolidColorBorderBrush", blackBrush);
+                    selectRightRect.StrokeThickness = 3;
+                    selectRightRect.Stroke = blackBrush;
+                    
+                    _callNumbersRight.Push(PopCallBlock(_callRecToMove));
+                    //Call animation (Move blocks)
+                    MessageBox.Show("top" + _callNumbersTop.Count + " vs right " + _callNumbersRight.Peek() +
+                                    " with count " + _callNumbersRight.Count);
+                }
+                else
+                {
+                    if (_callNumbersRight.Count < 5)//If there is still space for the block
+                    {
+                        if (_callNumbersRight.Count == 1)//If there is only one block inside
+                        { 
+                            SolidColorBrush blackBrush = new SolidColorBrush(Colors.Blue);
+                            this.RegisterName("MySolidColorBorderBrush", blackBrush);
+                            selectRightRect.StrokeThickness = 3;
+                            selectRightRect.Stroke = blackBrush;
+                            _callNumbersRight.Push(PopCallBlock(_callRecToMove)); //add second block
+                            if (_callNumbersRight.ElementAt(0) > _callNumbersRight.ElementAt(1))
+                            {
+                                _rectOrders[1] = false;//Store Descending for Right Rectangle
+                            }
+                            else
+                            {
+                                _rectOrders[1] = true;//Store Ascending for Right Rectangle
+                            }
+                            //Call animation (Move blocks)
+                            MessageBox.Show("top" + _callNumbersTop.Count + " vs right " + _callNumbersRight.Peek() +
+                                            " with count " + _callNumbersRight.Count);
+                        }
+                        else
+                        {
+                            if (PopCallBlock(_callRecToMove) > _callNumbersRight.Peek())//If new addition is greater than top num
+                            {
+                                _callNumbersRight.Push(PopCallBlock(_callRecToMove)); //add block
+                            }
+                            else
+                            {
+                                MessageBox.Show("This is an ascending list only X");
+                            }
+                            //What is my order
+                            if (_rectOrders[0])//If ascending
+                            {
+                                if (PopCallBlock(_callRecToMove) > _callNumbersRight.Peek())//If new addition is greater than top num
+                                {
+                                    _callNumbersRight.Push(PopCallBlock(_callRecToMove)); //add block
+                                }
+                                else
+                                {
+                                    MessageBox.Show("This is currently an ascending list");
+                                }
+                            }
+                            else
+                            {
+                                if (PopCallBlock(_callRecToMove) < _callNumbersRight.Peek())//If new addition is smaller than top num
+                                {
+                                    _callNumbersRight.Push(PopCallBlock(_callRecToMove)); //add block
+                                }
+                                else
+                                {
+                                    MessageBox.Show("This is currently a descending list");
+                                }
+                            }
+                            //Does it match my order?
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Space Error");
+                    }
+                }
+            }
         }
 
         private void selectLeftRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
-        }
+            bool isEmptyRect = !_callNumbersLeft.Any();//check if the list is empty
+            if (_activatedBlockCount == 0)//This is start block
+            {
+                //check num of blocks within rect
+                if (isEmptyRect)//No more blocks to move
+                {
+                    MessageBox.Show("Code X to appear and say empty");
+                }
+                else
+                {
+                    _callRecToMove = 2;//Represents Left
+                }
+                
+                _activatedBlockCount++;//Program now knows start, waiting for destination
+            }
+            else //this is destination block
+            {
+                //check num of blocks within rect
+                
+                if (isEmptyRect)//can add block
+                {
+                    _callNumbersLeft.Push(PopCallBlock(_callRecToMove));
+                }
+                else
+                {
+                    if (_callNumbersLeft.Count < 5)//If there is still space for the block
+                    {
+                        if (_callNumbersLeft.Count == 1)//If there is only one block inside
+                        { 
+                            _callNumbersLeft.Push(PopCallBlock(_callRecToMove)); //add second block
+                            if (_callNumbersLeft.ElementAt(0) > _callNumbersLeft.ElementAt(1))
+                            {
+                                _rectOrders[0] = false;//Store Descending for Left Rectangle
+                            }
+                            else
+                            {
+                                _rectOrders[0] = true;//Store Ascending for Left Rectangle
+                            }
+                        }
+                        else
+                        {
+                            if (PopCallBlock(_callRecToMove) > _callNumbersLeft.Peek())//If new addition is greater than top num
+                            {
+                                _callNumbersLeft.Push(PopCallBlock(_callRecToMove)); //add block
+                            }
+                            else
+                            {
+                                MessageBox.Show("This is an ascending list only X");
+                            }
+                            //What is my order
+                            if (_rectOrders[0])//If ascending
+                            {
+                                if (PopCallBlock(_callRecToMove) > _callNumbersLeft.Peek())//If new addition is greater than top num
+                                {
+                                    _callNumbersLeft.Push(PopCallBlock(_callRecToMove)); //add block
+                                }
+                                else
+                                {
+                                    MessageBox.Show("This is currently an ascending list");
+                                }
+                            }
+                            else
+                            {
+                                if (PopCallBlock(_callRecToMove) < _callNumbersLeft.Peek())//If new addition is smaller than top num
+                                {
+                                    _callNumbersLeft.Push(PopCallBlock(_callRecToMove)); //add block
+                                }
+                                else
+                                {
+                                    MessageBox.Show("This is currently a descending list");
+                                }
+                            }
+                            //Does it match my order?
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Space Error");
+                    }
+                }
+            }
+        }        
+        
+        private int PopCallBlock(int recLocation)
+                 {
+                     _activatedBlockCount = 0;
+                     switch (recLocation)
+                     {
+                         case 0:
+                             return _callNumbersTop.Pop();
+                         case 1:
+                             return _callNumbersBottom.Pop();
+                         case 2:
+                             return _callNumbersLeft.Pop();
+                         default:
+                             return _callNumbersRight.Pop();
+                     }
+                 }
+        private void checkRectangleOrder()
+                 {
+                     
+                 }
     }
 }
