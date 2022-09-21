147a40fca1c287f71969a3d01bd269a3e517c2b4
Josh <njmkhari@gmail.com>
2022-09-21 09:56:56 +0200

    Designing tutorial for Replacing books

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using JoshMkhariPROG7312Game.Logic.Home;
 
 namespace JoshMkhariPROG7312Game.Views
@@ -14,10 +19,24 @@ namespace JoshMkhariPROG7312Game.Views
         {
             InitializeComponent();
             _textBlockModel = new TextBlockModel();
+            ReplacingBooksBackground.Visibility = Visibility.Collapsed;
             foreach (TextBlock textBlock in TextBlockModel.TextBlocksList)
             {
                 WinHistoryStackPanel.Children.Add(textBlock);
             }
+        }
+
+        private void ReplacingBooksTutorial_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Hide all other elements
+            StackPanelBackground.Visibility = Visibility.Collapsed;
+            PageNameTitleTextBlock.Text = "Replacing books tutorial";
+            PageNameTitleTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            CanvasBack.Background =
+                new SolidColorBrush(Color.FromRgb(246,239,231)); //https://www.rapidtables.com/convert/color/hex-to-rgb.html
+            ReplacingBooksBackground.Visibility = Visibility.Visible;
+            
+            
         }
     }
 }
