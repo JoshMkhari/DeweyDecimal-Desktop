9651458f69f66bcea2c98f10df6d6ac462845fc0
unknown <njmkhari@gmail.com>
2022-10-21 16:23:14 +0200

    Identifying areas radio button click, changes view correctly

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -10,18 +10,24 @@ namespace JoshMkhariPROG7312Game.ViewModels
         {
             HomeVm = new HomeViewModel();
             ReplaceVm = new ReplaceBooksViewModel();
+            IdentifyingAreasVm = new IdentifyingAreasViewModel();
+            
             CurrentView = HomeVm;
 
             HomeViewCommand = new RelayCOmmand(o => { CurrentView = HomeVm; });
 
             ReplaceViewCommand = new RelayCOmmand(o => { CurrentView = ReplaceVm; });
+            
+            IdentifyingAreasCommand = new RelayCOmmand(o => { CurrentView = IdentifyingAreasVm; });
         }
 
         public RelayCOmmand HomeViewCommand { get; set; }
         public RelayCOmmand ReplaceViewCommand { get; set; }
+        public RelayCOmmand IdentifyingAreasCommand { get; set; }
 
         public HomeViewModel HomeVm { get; set; }
         public ReplaceBooksViewModel ReplaceVm { get; set; }
+        public IdentifyingAreasViewModel IdentifyingAreasVm { get; set; }
 
         public object CurrentView
         {
---
+++ Views/IdentifyingAreas.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace JoshMkhariPROG7312Game.Views
+{
+    public partial class IdentifyingAreas : UserControl
+    {
+        public IdentifyingAreas()
+        {
+            InitializeComponent();
+        }
+    }
+}
